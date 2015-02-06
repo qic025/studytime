6d69c035aca247ce63640152290ea2d8d8820fd5
vagrant <vagrant@precise32.(none)>
2015-02-06 22:11:18 +0000

    Moved the css and js directories into static

---
+++ static/js/studytime.js
@@ -0,0 +1,19 @@
+/*
+function verticalCenterStuff() {
+  var windowHeight = $(window).height();
+  var loginBoxHeight = $('#login-form').innerHeight();
+  var mathLogin = (windowHeight / 2) - (loginBoxHeight / 2);
+  $('#login-form').css('margin-top', mathLogin);
+}
+
+$(window).resize(function() {
+  verticalCenterStuff();
+});
+verticalCenterStuff();
+*/
+
+$('#btn-login').click(function() {
+  $(this).parent().fadeOut(function() {
+    $('#login-form').fadeIn();
+  })
+});
